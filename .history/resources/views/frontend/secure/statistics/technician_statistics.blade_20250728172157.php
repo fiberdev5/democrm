1648f0f167f2b5ab0a5207b1d50a364d4d72e456
fiberdev6 <fiberdev6@gmail.com>
2025-10-06 14:08:19 +0300

    css duzenlemeleri ve sorgu optimizasyonu yapıldı

--- .history/resources/views/frontend/secure/statistics/technician_statistics.blade_20250728172157.php
+++
@@ -1,8 +0,0 @@
-@extends('frontend.secure.user_master')
-@section('user')
-<div class="page-content">
-    <div class="container-fluid">
-        @include('frontend.secure.statistics.statistics_menu', ['tenant_id' => $tenant_id])
-    </div>
-</div>
-@endsection
