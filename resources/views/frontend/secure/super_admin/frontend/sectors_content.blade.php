38f7b64b31b206ef533c651222f9f27b37cf3914
Meryem <mac@air-meryem.local>
2025-12-22 10:24:50 +0300

    meryem temiz

--- resources/views/frontend/secure/super_admin/frontend/sectors_content.blade.php
+++ resources/views/frontend/secure/super_admin/frontend/sectors_content.blade.php
@@ -3,6 +3,7 @@
 <div class="page-content">
     <div class="container-fluid">
         
+        
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
